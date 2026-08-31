76b52909426810c73708b7926b78279b292c7879
Cathal Diamond <cathaldiamond1@gmail.com>
2026-08-31 12:55:37 +0100

    feat: improve CLI output and safe file naming

--- src/main/java/io/github/cdiamondgit/securefile/FileCryptoService.java
+++ src/main/java/io/github/cdiamondgit/securefile/FileCryptoService.java
@@ -22,9 +22,12 @@ public class FileCryptoService {
     private static final int NONCE_BYTES = 12;
 
     public void encrypt(Path path, char[] password) throws GeneralSecurityException, IOException { // throw the exceptions back to main
+
         byte[] saltBytes = generateSalt();
         SecretKey AESkey = generateAESKey(saltBytes, password);
-        byte[] nonceBytes = generateNonce(); 
+        byte[] nonceBytes = generateNonce();
+
+        System.out.println("Encrypting...\n");
 
         Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding"); // create cipher encryption engine 
         GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, nonceBytes); // The 128 bit GCM authentication tag verifies that the ciphertext has not been altered and that the correct key and nonce were used
@@ -45,46 +48,21 @@ public class FileCryptoService {
         buffer.put(encryptedBytes);
 
         Files.write(outputPath, buffer.array());
-    }
 
-    private byte[] generateSalt() { // random array of bytes are needed (salt), as password + salt -> hash
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] saltBytes = new byte[SALT_BYTES]; 
+        String originalFileSize = formatFileSize(Files.size(path));
+        String newFileSize = formatFileSize(Files.size(outputPath));
 
-        secureRandom.nextBytes(saltBytes);
-    
-        return saltBytes;
-    }
+        System.out.println("Input: " + path.getFileName().toString() + " (" + originalFileSize + ")");
+        System.out.println("Output: " + outputPath.getFileName().toString() + " (" + newFileSize + ")");
 
-    private SecretKey generateAESKey(byte[] saltBytes, char[] password) throws GeneralSecurityException { // if the SecretKeyFactory API cant get the algorithm, it is passed back to the method that called it
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password, saltBytes, 210000, 256); // Password-Based Encryption Key Specification. It is a container that contains info about the key to be generated
-
-        try {
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256"); // PBKDF... is the cryptographic method to be used to generate the AES key
-            SecretKey derivedKey = secretKeyFactory.generateSecret(pbeKeySpec); // generate 256 bits of derived secret key material
-
-            byte[] keyBytes = derivedKey.getEncoded(); // turn the derivedKey into a simple array of bytes
-            SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES"); // It was not yet explicitly an AES key, so its bytes now will be wrapped in a SecretKeySpec configured for AES
-            return secretKeySpec; // secretKeySpec is a type of SecretKey, except explicitly an AES key so it can be returned as a SecretKey
-        } finally {
-            pbeKeySpec.clearPassword(); // get rid of password data from memory
-        }
-    }
-
-    private byte[] generateNonce() { // The nonce gives a unique starting value for each encryption so the same key does not reuse the same encryption stream
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] nonceBytes = new byte[NONCE_BYTES]; 
-
-        secureRandom.nextBytes(nonceBytes);
-
-        return nonceBytes;
+        System.out.println("\nEncryption successful\n");
     }
 
     public void decrypt(Path path, char[] password) throws GeneralSecurityException, IOException { // throw the exceptions back to main
         byte[] readBytes = Files.readAllBytes(path);
         int minimumSize = MAGIC_BYTES.length + Byte.BYTES + SALT_BYTES + NONCE_BYTES + 16; // 16 as the GCM tag is 16 bytes (128 / 8)
         if (readBytes.length < minimumSize) {
-            System.out.println("Error: Invalid encrypted file");
+            System.out.println("\nError: Invalid encrypted file\n");
             return;
         }
 
@@ -107,14 +85,107 @@ public class FileCryptoService {
             SecretKey recreatedAESkey = generateAESKey(readSaltBytes, password); // The same AES key is created from the password and salt bytes
             Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding"); 
             GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, readNonceBytes); // The 128 bit GCM authentication tag verifies that the ciphertext has not been altered and that the correct key and nonce were used
+
             cipher.init(Cipher.DECRYPT_MODE, recreatedAESkey, gcmParameterSpec); 
 
+            System.out.println("Decrypting...\n");
+
             byte[] decryptedBytes = cipher.doFinal(readFileContentsAndTag);
-            Path outputPath = path.resolveSibling(path.getFileName().toString() + ".dec");
+            String inputFileName = path.getFileName().toString();
+            String outputName = fileNameHandler(inputFileName, path);
+            Path outputPath = path.resolveSibling(outputName);
+
             Files.write(outputPath, decryptedBytes);
+
+            String originalFileSize = formatFileSize(Files.size(path));
+            String newFileSize = formatFileSize(Files.size(outputPath));
+
+            System.out.println("Input: " + path.getFileName().toString() + " (" + originalFileSize + ")");
+            System.out.println("Output: " + outputPath.getFileName().toString() + " (" + newFileSize + ")");
+
+            System.out.println("\nDecryption successful\n");
         } else {
-            System.out.println("Error: Could not open encrypted file");
+            System.out.println("\nError: Could not open encrypted file\n");
             return;
         }
+    }
+
+    private byte[] generateSalt() { // random array of bytes are needed (salt), as password + salt -> hash
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] saltBytes = new byte[SALT_BYTES]; 
+
+        secureRandom.nextBytes(saltBytes);
+
+        return saltBytes;
+    }
+
+    private SecretKey generateAESKey(byte[] saltBytes, char[] password) throws GeneralSecurityException { // if the SecretKeyFactory API cant get the algorithm, it is passed back to the method that called it
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(password, saltBytes, 600000, 256); // Password-Based Encryption Key Specification. It is a container that contains info about the key to be generated
+
+        try {
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256"); // PBKDF... is the cryptographic method to be used to generate the AES key
+            SecretKey derivedKey = secretKeyFactory.generateSecret(pbeKeySpec); // generate 256 bits of derived secret key material
+
+            byte[] keyBytes = derivedKey.getEncoded(); // turn the derivedKey into a simple array of bytes
+            SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES"); // It was not yet explicitly an AES key, so its bytes now will be wrapped in a SecretKeySpec configured for AES
+            return secretKeySpec; // secretKeySpec is a type of SecretKey, except explicitly an AES key so it can be returned as a SecretKey
+        } finally {
+            pbeKeySpec.clearPassword(); // get rid of password data from memory
+        }
+    }
+
+    private byte[] generateNonce() { // The nonce gives a unique starting value for each encryption so the same key does not reuse the same encryption stream
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] nonceBytes = new byte[NONCE_BYTES]; 
+
+        secureRandom.nextBytes(nonceBytes);
+
+        return nonceBytes;
+    }
+
+    private static String formatFileSize(long bytes) {
+        if (bytes >= 1000 * 1000 * 1000) {
+            return String.format("%.1f GB", bytes / (1000.0 * 1000 * 1000));
+        } else if (bytes >= 1000 * 1000) {
+            return String.format("%.1f MB", bytes / (1000.0 * 1000));
+        } else if (bytes >= 1000) {
+            return String.format("%.1f KB", bytes / 1000.0);
+        } else {
+            return bytes + " bytes";
+        }
+    }
+
+
+    private String fileNameHandler(String inputFileName, Path path) {
+        String outputName;
+        if (inputFileName.endsWith(".sec")) {
+            outputName = inputFileName.substring(0, inputFileName.length() - ".sec".length()); // remove .sec from the encrypted file name
+        } else {
+            outputName = path.getFileName().toString(); // otherwise just take the same name as the file 
+        }
+
+        Path outputPath = path.resolveSibling(outputName);
+
+        if (outputName.indexOf('.') != -1) { // if the file has an extension
+            String outputFileExtension = outputName.substring(outputName.lastIndexOf('.'),outputName.length());
+            String outputFileMinusExtension = outputName.substring(0,outputName.lastIndexOf('.'));
+
+
+            int i = 1;
+            while (Files.exists(outputPath)) {
+                outputName = outputFileMinusExtension + " (" + i + ")" + outputFileExtension.toString();
+                outputPath = path.resolveSibling(outputName);
+                i++;
+            }
+        } else { // if the file does not have an extension
+            String outputFileOriginalName = outputName;
+            int i = 1;
+            while (Files.exists(outputPath)) {
+                outputName = outputFileOriginalName + " (" + i + ")" ;
+                outputPath = path.resolveSibling(outputName);
+                i++;
+            }
+        }
+        return outputName;
     }
 }
--- src/main/java/io/github/cdiamondgit/securefile/Main.java
+++ src/main/java/io/github/cdiamondgit/securefile/Main.java
@@ -13,32 +13,32 @@ public class Main {
         Path path;
 
         if (args.length == 0) {
-            System.out.println("Error: No arguments passed");
+            System.out.println("\nError: No arguments passed\n");
             return;
         }
 
         if (args[0].equals("encrypt") || args[0].equals("decrypt")) {
             if (args.length > 2) {
-                System.out.println("Error: Too many arguments passed");
+                System.out.println("Error: Too many arguments passed\n");
                 return;
             } else if (args.length == 1) {
-                System.out.println("Error: No file given");
+                System.out.println("Error: No file given\n");
                 return;
             } else {
                 file = args[1];
             }
         } else {
-            System.out.println("Error: Incorrect command");
+            System.out.println("Error: Incorrect command\n");
             return;
         }
 
         path = Path.of(file);
 
         if (Files.notExists(path)) {
-            System.out.println("Error: File does not exist");
+            System.out.println("Error: File does not exist\n");
             return;
         } else if (!(Files.isRegularFile(path))){
-            System.out.println("Error: File is not a regular file");
+            System.out.println("Error: File is not a regular file\n");
             return;
         }
 
@@ -48,7 +48,7 @@ public class Main {
             Console console = System.console();
 
             if (console == null) {
-                System.out.println("Error: Console could not be accessed");
+                System.out.println("Error: Console could not be accessed\n");
                 return;
             }
 
@@ -56,14 +56,14 @@ public class Main {
             password = console.readPassword("Password: ");
 
             if (password == null) {
-                System.out.println("Error: Password could not be read");
+                System.out.println("Error: Password could not be read\n");
                 return;
             }
 
             try {
                 fileCryptoService.encrypt(path, password);
             } catch (GeneralSecurityException | IOException e) {
-                System.out.println("Error: Cryptographic operation failed: " + e.getMessage());
+                System.out.println("Error: Cryptographic operation failed: " + e.getMessage() + "\n");
             } finally {
                 Arrays.fill(password, '\0'); // whether fileCryptoService runs or not, the password will be wiped from memory
             }
@@ -73,7 +73,7 @@ public class Main {
             Console console = System.console();
 
             if (console == null) {
-                System.out.println("Error: Console could not be accessed");
+                System.out.println("Error: Console could not be accessed\n");
                 return;
             }
 
@@ -81,14 +81,14 @@ public class Main {
             password = console.readPassword("Password: ");
 
             if (password == null) {
-                System.out.println("Error: Password could not be read");
+                System.out.println("Error: Password could not be read\n");
                 return;
             }
 
             try {
                 fileCryptoService.decrypt(path, password);
             } catch (GeneralSecurityException | IOException e) {
-                System.out.println("Error: Cryptographic operation failed: " + e.getMessage());
+                System.out.println("Error: Cryptographic operation failed: " + e.getMessage() + "\n");
             } finally {
                 Arrays.fill(password, '\0'); // whether fileCryptoService runs or not, the password will be wiped from memory
             }
